79fa8a20afa93863796680e1e489d86de5a1fd8f
FrancisMaynard <francismaynard99@gmail.com>
2023-07-12 11:49:13 -0400

    Un ptit cleanup

--- rodrigo_backend/routes/commentRouter.js
+++ rodrigo_backend/routes/commentRouter.js
@@ -13,9 +13,9 @@ router.get('/:id', (req, res, next) => {
         return next(new HttpError(400, 'Le champ recetteId est requis'));
     }
 
-    recetteQueries.getRecetteById(id).then(recette => {
+    recetteQueries.getRecetteById(recetteId).then(recette => {
         if (!recette) {
-            return next(new HttpError(404, `La recette ${id} est inexistante ou introuvable`));
+            return next(new HttpError(404, `La recette ${recetteId} est inexistante ou introuvable`));
         }
     }).catch(err => {
         return next(err);
@@ -36,36 +36,36 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.get('/:id/:user', (req, res, next) => {
-    const id = req.params.id;
+    const recetteId = req.params.id;
     const user = req.params.user;
 
-    if (id == null || id === "") {
-        return next(new HttpError(400, `Le parametre Id est requis`));
+    if (recetteId == null || recetteId === "") {
+        return next(new HttpError(400, `Le parametre recetteId est requis`));
     }
     if (user == null || user === "") {
         return next(new HttpError(400, `Le parametre user est requis`));
     }
 
-    recetteQueries.getRecetteById(id).then(recette => {
+    recetteQueries.getRecetteById(recetteId).then(recette => {
         if (!recette) {
-            return next(new HttpError(404, `La recette ${id} est inexistante ou introuvable`));
+            return next(new HttpError(404, `La recette ${recetteId} est inexistante ou introuvable`));
         }
     }).catch(err => {
         return next(err);
     });
 
     if (user != req.user.courrielUtilisateur) {
-        return next(new HttpError(403, `Vous devez être le même utilisateur pour voir l'appréciation`));
+        return next(new HttpError(403, `Vous devez être le même utilisateur pour voir le commentaire`));
     }
 
-    commentQueries.getUserCommentByRecetteId(id, user).then(commentaire => {
+    commentQueries.getUserCommentByRecetteId(recetteId, user).then(commentaire => {
         if (commentaire) {
             if (commentaire.result == 0) {
-                return next(new HttpError(404, `La recette ${id} est introuvable ou n'existe pas`));
+                return next(new HttpError(404, `La recette ${recetteId} est introuvable ou n'existe pas`));
             }
             res.json(commentaire);
         } else {
-            return next(new HttpError(404, `Commentaire de ${user} pour la recette ${id} introuvable`));
+            return next(new HttpError(404, `Commentaire de ${user} pour la recette ${recetteId} introuvable`));
         }
     }).catch(err => {
         return next(err);

96747056a77379ee4f2cced485f1850397f5a8da
FrancisMaynard <francismaynard99@gmail.com>
2023-07-14 10:00:58 -0400

    Formattage CommentDate FrontEnd

--- rodrigo_frontend/src/pages/DetailedRecipe/Commentaires/CommentaireRecette.vue
+++ rodrigo_frontend/src/pages/DetailedRecipe/Commentaires/CommentaireRecette.vue
@@ -1,14 +1,24 @@
 <template>
     <v-card class="pa-2 ma-2">
-        <span><strong>{{ commentaire.nomUtilisateur }} ({{ commentaire.commentDatePublication }})</strong></span>
+        <span><strong>{{ commentaire.nomUtilisateur }} ({{ formattedDateTime }})</strong></span>
         <div>{{ commentaire.commentUtilisateur }} </div>
     </v-card>
 </template>
 
 <script>
+import { DateTime } from 'luxon';
 export default {
     props: {
         commentaire: Object,
+    },
+    computed: {
+        formattedDateTime() {
+            const commentaireDateTime = DateTime.fromISO(this.commentaire.commentDatePublication);
+
+            const commentaireDateTimeFR = commentaireDateTime.setLocale('fr').toLocaleString(DateTime.DATETIME_MED);
+
+            return commentaireDateTimeFR;
+        }
     }
 }
 </script>

c3c9360cc58ad70a68fd687b949bc13b83b4c4ab
Marchand007 <m.marchand22@hotmail.com>
2023-07-14 10:56:48 -0400

    dernie update

--- rodrigo_frontend/src/pages/FormulaireNouvUser.vue
+++ rodrigo_frontend/src/pages/FormulaireNouvUser.vue
@@ -2,21 +2,23 @@
     <div class="boxed-center">
         <v-sheet class="ma-2" max-width="40rem">
             <v-sheet class="ma-2" max-width="40rem">
-                <v-form @submit.prevent="createUserAccount" validate-on="submit lazy" ref="newUserForm">
+                <v-form @submit.prevent="createUserAccount" validate-on="blur" ref="newUserForm">
                     <v-text-field v-model="userAccountEmail" label="Adresse courriel"
-                        :rules="[rules.required, rules.validEmail, rules.userAccountEmailUnique]" density="compact">
+                        :rules="[rules.required, rules.validEmail, rules.userAccountEmailUnique]" density="compact"
+                        clearable>
                     </v-text-field>
 
-                    <v-text-field v-model="userFullName" label="Nom complet" 
-                        :rules="[rules.required, rules.validFullName]">
+                    <v-text-field v-model="userFullName" label="Nom complet" :rules="[rules.required, rules.validFullName]"
+                        clearable>
                     </v-text-field>
 
-                    <v-text-field v-model="password" label="Mot de passe" type="password"
-                        :rules="[rules.required, rules.validPassword]" density="compact">
+                    <v-text-field @input="validatePassword" v-model="password" label="Mot de passe" type="password"
+                        :rules="[rules.required, rules.validPassword]" density="compact" ref="passwordInput" clearable>
                     </v-text-field>
 
-                    <v-text-field v-model="passwordConf" label="Confirmer le mot de passe"
-                        :rules="[rules.required, rules.passwordsMatch]" type="password" density="compact">
+                    <v-text-field @input="validatePasswordMatch" v-model="passwordConf" label="Confirmer le mot de passe"
+                        :rules="[rules.required, rules.passwordsMatch]" type="password" density="compact"
+                        ref="passwordConfirmInput" clearable>
                     </v-text-field>
 
                     <v-btn type="submit" :disabled="!userAccountEmail || !userFullName || !password || !passwordConf">
@@ -31,7 +33,8 @@
 import session from '../session';
 
 export default {
-    data: function () {
+    data: function ()
+    {
         return {
             userAccountEmail: '',
             password: '',
@@ -39,15 +42,18 @@ export default {
             userFullName: '',
             rules: {
                 required: value => !!value || "Le champ est requis",
-                validEmail: value => {
+                validEmail: value =>
+                {
                     const validEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
                     return validEmail.test(value) || "Veuillez entrer une adresse courriel valide"
                 },
-                validFullName: value => {
+                validFullName: value =>
+                {
                     const validFullName = /(^[A-Z][a-z]+)(-){0,1}([ ]{0,1})([A-Z][a-z]+)?([ ]{1})?([A-Z][a-z]+)(-{0,1})([ ]{0,1})([A-Z][a-z]+)?$/;
                     return validFullName.test(value) || "Veuillez entrer un nom complet valide, (première lettre par mot doit être une majuscule)"
                 },
-                validPassword: value => {
+                validPassword: value =>
+                {
                     const validPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
                     return validPasswordRegex.test(value) || "Le mot de passe doit contenir au moins 8 caractères, 1 majuscule, 1 chiffre et 1 caractère spécial"
                 },
@@ -58,32 +64,44 @@ export default {
         };
     },
     methods: {
-        async createUserAccount() {
+        async createUserAccount()
+        {
             this.userAccountUnique = true;
             const formValid = await this.$refs.newUserForm.validate();
 
-            if (formValid.valid) {
+            if (formValid.valid)
+            {
                 session.createUserAccount(this.userAccountEmail, this.userFullName, this.password).then(
-                    () => {
+                    () =>
+                    {
                         alert(`Compte créé avec succès ! Bienvenue ${this.userFullName} !`);
                         this.userAccountUnique = true;
                         session.login(this.userAccountEmail, this.password);
                         this.$router.replace('/');
-                    }).catch(authError => {
+                    }).catch(authError =>
+                    {
                         alert(authError.message);
-                        if (authError.status === 409) {
+                        if (authError.status === 409)
+                        {
                             this.userAccountUnique = false;
                             this.$refs.newUserForm.validate();
                         }
                     });
             }
+        },
+        validatePassword()
+        {
+            this.$refs.passwordInput.validate();
+        },
+        validatePasswordMatch()
+        {
+            this.$refs.passwordConfirmInput.validate();
         }
     }
 }
 </script>
 
-<style scoped>
-.boxed-center {
+<style scoped>.boxed-center {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     margin: 1rem auto;
     border-radius: 10px;
@@ -91,5 +109,4 @@ export default {
     text-align: center;
     width: 40%;
     max-width: 80rem;
-}
-</style>
+}</style>
